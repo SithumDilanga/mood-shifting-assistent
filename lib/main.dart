c28420940c47c505f4be5654231a616ee3751870
SithumDilanga <sithum.98dilanga@gmail.com>
2022-02-10 00:57:12 +0530

    💁tensorflow model configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mood_shifting_assistent/text_classification.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,13 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,24 +40,22 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ElevatedButton(
+              child: const Text(
+                'Text classification'
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const TextClassification()),
+                );
+              }, 
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
---
+++ lib/text_classification.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:mood_shifting_assistent/classifier.dart';
+
+class TextClassification extends StatefulWidget {
+  const TextClassification({ Key? key }) : super(key: key);
+
+  @override
+  _TextClassificationState createState() => _TextClassificationState();
+}
+
+class _TextClassificationState extends State<TextClassification> {
+
+  late TextEditingController _controller;
+  late Classifier _classifier;
+  late List<Widget> _children;
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _classifier = Classifier();
+    _children = [];
+    _children.add(Container());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child : Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orangeAccent,
+          title: const Text('Text classification'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(4),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                  child: ListView.builder(
+                itemCount: _children.length,
+                itemBuilder: (_, index) {
+                  return _children[index];
+                },
+              )),
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.orangeAccent)),
+                child: Row(children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                          hintText: 'Write some text here'),
+                      controller: _controller,
+                    ),
+                  ),
+                  TextButton(
+                    child: const Text('Classify'),
+                    onPressed: () {
+                      
+                      final text = _controller.text;
+                      final prediction = _classifier.classify(text);
+
+                      setState(() {
+                        _children.add(Dismissible(
+                          key: GlobalKey(),
+                          onDismissed: (direction) {},
+                          child: Card(
+                            child: Container(
+                              padding: const EdgeInsets.all(16),
+                              color: prediction[1] > prediction[0]
+                                  ? Colors.lightGreen
+                                  : Colors.redAccent,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    "Input: $text",
+                                    style: const TextStyle(fontSize: 16),
+                                  ),
+                                  Text("Output:"),
+                                  Text("   Positive: ${prediction[1]}"),
+                                  Text("   Negative: ${prediction[0]}"),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ));
+                        _controller.clear();
+                      });
+                    },
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
