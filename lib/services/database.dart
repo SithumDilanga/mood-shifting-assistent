0a444cd2166b0c8da4f6e39cfd3d45f49c9d9b86
SithumDilanga <sithum.98dilanga@gmail.com>
2022-02-18 00:07:29 +0530

    👾daily progress send to db,weekly progress send to db

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -10,8 +10,104 @@ class DatabaseService {
     return userCollection.doc(uid).collection('dailyProgress').doc().set({
       'journalText': 'test text',
       'status': 'positive',
-      'statusCalculation': '0.56'
+      'statusCalculation': 0.41,
+      'timeStamp': FieldValue.serverTimestamp(),
+      'order': FieldValue.increment(1)
     });
+
+  }
+
+  Future getDailyProgress(String userId,) async {
+
+    return userCollection.doc(userId).collection('dailyProgress').get().then((value) {
+      print('value ${value.docs.length}');
+
+      if(value.docs.length % 7 == 0) {
+
+        print('week completed!');
+
+        getWeekFromDailyProgress(userId);
+
+      }
+
+      return value.docs.length;
+    } 
+
+    );
+
+  }
+
+  Future sendWeeklyProgress(String uid, double weeklyProgress, String weeklyStatus) async {
+
+    return userCollection.doc(uid).collection('weeklyProgress').doc().set({
+      'weeklyStatus': weeklyStatus,
+      'weeklyProgress': weeklyProgress,
+      'timeStamp': FieldValue.serverTimestamp(),
+    });
+
+  }
+
+  Future getWeekFromDailyProgress(String userId,) async {
+
+    // DateTime _now = DateTime.now();
+    // DateTime _start = DateTime(_now.year, _now.month, _now.day, 0, 0);
+    // DateTime _end = DateTime(_now.year, _now.month, _now.day, 23, 59, 59);
+
+    var dailProgress = [];
+    double average = 0;
+    double totalProgrss = 0;
+    String weeklyStatus = '';
+
+    await FirebaseFirestore.instance.collection('users').doc('Rfpm4kAiYKVwJcNNomVI').collection('dailyProgress')
+    .orderBy('timeStamp', descending: true)
+    .where(
+      'timeStamp', isLessThan: DateTime.now()
+    ).get().then((QuerySnapshot querySnapshot) {
+
+      for(int i = 0; i < querySnapshot.docs.length; i++) {
+
+        if(i < 7) {
+
+          dailProgress.add(querySnapshot.docs[i]);
+          // var date = DateTime.parse(querySnapshot.docs[i]['timeStamp'].toDate().toString());
+
+          totalProgrss = totalProgrss + querySnapshot.docs[i]['statusCalculation'];
+
+          print('dailyProgress ${querySnapshot.docs[i]['statusCalculation']}');
+
+        } else {
+
+          print('totalProgrss $totalProgrss');
+
+          average = totalProgrss / 7;
+
+          if(average >= 0.5) {
+            weeklyStatus = 'positive';
+          } else {
+            weeklyStatus = 'negative';
+          }
+
+          sendWeeklyProgress(userId, average, weeklyStatus);
+
+          print('average $average');
+
+          print('exceeds the limit!');
+
+          return average;
+
+        }
+        
+      }
+
+        // querySnapshot.docs.forEach((doc) {
+        //   dailProgress.add(doc);
+        //   var date = DateTime.parse(doc['timeStamp'].toDate().toString());
+        //   print('dailProgress $date');
+        // })
+
+    });
+
+    return average;
 
   }
 
--- lib/text_classification.dart
+++ lib/text_classification.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mood_shifting_assistent/classifier.dart';
 import 'package:mood_shifting_assistent/services/database.dart';
@@ -34,79 +35,113 @@ class _TextClassificationState extends State<TextClassification> {
           backgroundColor: Colors.orangeAccent,
           title: const Text('Text classification'),
         ),
-        body: Container(
-          padding: const EdgeInsets.all(4),
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                  child: ListView.builder(
-                itemCount: _children.length,
-                itemBuilder: (_, index) {
-                  return _children[index];
-                },
-              )),
-              ElevatedButton(
-                child: const Text(
-                  'Send'
-                ),
-                onPressed: () {
+        body: FutureBuilder(
+          future: null,//_databaseService.getWeekFromDailyProgress('Rfpm4kAiYKVwJcNNomVI'),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
 
-                  _databaseService.sendDailyProgress('Rfpm4kAiYKVwJcNNomVI');
+            if (snapshot.hasData) {
 
-                }, 
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.orangeAccent)),
-                child: Row(children: <Widget>[
+              print('yoyo ${snapshot.data[0]['status']}');
+
+            }
+
+            return Container(
+              padding: const EdgeInsets.all(4),
+              child: Column(
+                children: <Widget>[
                   Expanded(
-                    child: TextField(
-                      decoration: const InputDecoration(
-                          hintText: 'Write some text here'),
-                      controller: _controller,
-                    ),
-                  ),
-                  TextButton(
-                    child: const Text('Classify'),
-                    onPressed: () {
-                      
-                      final text = _controller.text;
-                      final prediction = _classifier.classify(text);
-
-                      setState(() {
-                        _children.add(Dismissible(
-                          key: GlobalKey(),
-                          onDismissed: (direction) {},
-                          child: Card(
-                            child: Container(
-                              padding: const EdgeInsets.all(16),
-                              color: prediction[1] > prediction[0]
-                                  ? Colors.lightGreen
-                                  : Colors.redAccent,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    "Input: $text",
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                  Text("Output:"),
-                                  Text("   Positive: ${prediction[1]}"),
-                                  Text("   Negative: ${prediction[0]}"),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ));
-                        _controller.clear();
-                      });
+                      child: ListView.builder(
+                    itemCount: _children.length,
+                    itemBuilder: (_, index) {
+                      return _children[index];
                     },
+                  )),
+                  ElevatedButton(
+                    child: const Text(
+                      'Send'
+                    ),
+                    onPressed: () async {
+
+                      _databaseService.sendDailyProgress('Rfpm4kAiYKVwJcNNomVI');
+                      // _databaseService.getWeekFromDailyProgress('Rfpm4kAiYKVwJcNNomVI');
+
+                      // await FirebaseFirestore.instance.collection('users').doc('Rfpm4kAiYKVwJcNNomVI').collection('dailyProgress').get().then((QuerySnapshot querySnapshot) => {
+                      //   querySnapshot.docs.forEach((doc) {
+                      //     // dailProgress.add(doc);
+                      //     print('dailProgress ${doc['status']}');
+                      //   })
+                      // });
+
+                    }, 
                   ),
-                ]),
+                  ElevatedButton(
+                    child: const Text(
+                      'get dailyPosts'
+                    ),
+                    onPressed: () async {
+
+                      // dynamic length = await _databaseService.getDailyProgress('Rfpm4kAiYKVwJcNNomVI');
+
+                      // print('length $length');
+
+                      _databaseService.getWeekFromDailyProgress('Rfpm4kAiYKVwJcNNomVI');
+
+                    }, 
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.orangeAccent)),
+                    child: Row(children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          decoration: const InputDecoration(
+                              hintText: 'Write some text here'),
+                          controller: _controller,
+                        ),
+                      ),
+                      TextButton(
+                        child: const Text('Classify'),
+                        onPressed: () {
+                          
+                          final text = _controller.text;
+                          final prediction = _classifier.classify(text);
+
+                          setState(() {
+                            _children.add(Dismissible(
+                              key: GlobalKey(),
+                              onDismissed: (direction) {},
+                              child: Card(
+                                child: Container(
+                                  padding: const EdgeInsets.all(16),
+                                  color: prediction[1] > prediction[0]
+                                      ? Colors.lightGreen
+                                      : Colors.redAccent,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        "Input: $text",
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                      Text("Output:"),
+                                      Text("   Positive: ${prediction[1]}"),
+                                      Text("   Negative: ${prediction[0]}"),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ));
+                            _controller.clear();
+                          });
+                        },
+                      ),
+                    ]),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          }
         ),
       ),
     );
